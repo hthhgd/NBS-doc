8edce16121f9d410793f6dad6b4be4a6568ae00c
hthhgd <101369723+hthhgd@users.noreply.github.com>
2022-07-13 15:57:17 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NBS官方文档'
+copyright = '2022,hthhgd'
+author = 'hthhgd'
 
 release = '0.1'
 version = '0.1.0'
